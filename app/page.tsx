e79ec884696a0c52a05b6d7e9b953dd3f07f0ced
Nameless <nameless@thenamelessdev.com>
2026-05-13 16:37:44 +0200

    feat: add movie card

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
+import MovieCard from '@/components/MovieCard';
 import React from 'react'
 
 export default function MainPage() {
-  return (
-    <div>MainPage</div>
-  )
+  return (<>
+    <MovieCard title="Testing" description="This is a movie for testing" imageUrl="https://media1.tenor.com/m/qMH5o_XizbcAAAAd/but-here%27s-the-coder.gif"/>
+  </>)
 }
---
+++ components/MovieCard.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function MovieCard({title, description, imageUrl}: {title: string, description: string, imageUrl: string}) {
+  return (<>
+    <div className="border-2 rounded w-fit p-5 text-center bg-cover" style={{backgroundImage: `url("${imageUrl}")`}}>
+        <p className="text-lg font-bold">{title}</p>
+        <p>{description}</p>
+    </div>
+  </>)
+}
