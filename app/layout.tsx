241d0591724e0cb2c664dc230c202c9c943baf25
Nameless <nameless@thenamelessdev.com>
2026-05-13 16:38:38 +0200

    feat: change bg color

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css"
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-neutral-800 text-neutral-300">
         <main className="m-3">
           {children}
         </main>
