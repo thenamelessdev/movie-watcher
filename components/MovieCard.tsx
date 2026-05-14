6ea926e4ac593dbb18ff6600484064012969f23a
Nameless <nameless@thenamelessdev.com>
2026-05-14 16:33:22 +0200

    remove: movie card border radius

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function MovieCard({title, description, imageUrl}: {title: string, description: string, imageUrl: string}) {
   return (<>
-    <div className="border-2 border-black rounded w-fit p-5 text-center bg-cover" style={{backgroundImage: `url("${imageUrl}")`}}>
+    <div className="border-2 border-black w-fit p-5 text-center bg-cover" style={{backgroundImage: `url("${imageUrl}")`}}>
         <p className="text-lg font-bold">{title}</p>
         <p>{description}</p>
     </div>
